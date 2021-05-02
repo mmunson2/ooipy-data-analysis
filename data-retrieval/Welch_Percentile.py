e74e2a09b18b04d51b947b7f7a1c46773d42aaee
FelixSchwock <fschwock@gmail.com>
2021-05-01 22:39:36 -0700

    fixed mistake

--- data-retrieval/Welch_Percentile.py
+++ data-retrieval/Welch_Percentile.py
@@ -124,10 +124,9 @@ def compute_spectrogram_wp(data, stats, win='hann', L=4096, avg_time=None,
                 sense_corr = -frequency_calibration(stats, int(L / 2 + 1))
 
                 Pxx = 10 * np.log10(Pxx * np.power(10, sense_corr / 10))
-
                 specgram.append(Pxx)
                 time.append(stats.starttime.datetime
-                            + datetime.timedelta(seconds=n * L / fs))
+                            + datetime.timedelta(seconds=n * avg_time))
 
         # compute PSD for residual segment
         # if segment has more than L samples
@@ -150,10 +149,10 @@ def compute_spectrogram_wp(data, stats, win='hann', L=4096, avg_time=None,
                 sense_corr = -frequency_calibration(stats, int(L / 2 + 1))
 
                 Pxx = 10 * np.log10(Pxx * np.power(10, sense_corr / 10))
-
                 specgram.append(Pxx)
                 time.append(stats.starttime.datetime
-                            + datetime.timedelta(seconds=n * L / fs))
+                            + datetime.timedelta(seconds=(nbins - 1)
+                                                 * avg_time))
 
     if len(time) == 0:
         if verbose:
